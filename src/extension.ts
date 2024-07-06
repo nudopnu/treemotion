e2b9807cf9181be81a452ef767a8b3130bd94a22
nudopnu <forsuse@live.de>
2024-07-06 15:23:03 +0200

    Added basic vim mode

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let vimMode: 'insert' | 'command' = 'insert';
+
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('treemotions.toggleMode', toggleMode)
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('treemotions.navigateLeft', () => navigate('left'))
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('treemotions.navigateDown', () => navigate('down'))
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('treemotions.navigateUp', () => navigate('up'))
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('treemotions.navigateRight', () => navigate('right'))
+	);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "treemotions" is now active!');
+	vscode.window.onDidChangeTextEditorSelection(updateCursor, null, context.subscriptions);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('treemotions.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TreeMotions!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function toggleMode() {
+	vimMode = vimMode === 'insert' ? 'command' : 'insert';
+	vscode.commands.executeCommand('setContext', 'vimMode', vimMode);
+	updateCursor();
+}
+
+function navigate(direction: 'left' | 'down' | 'up' | 'right') {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor || vimMode !== 'command') return;
+
+	const positions = editor.selections.map(selection => {
+		const position = selection.active;
+		switch (direction) {
+			case 'left': return position.translate(0, -1);
+			case 'down': return position.translate(1, 0);
+			case 'up': return position.translate(-1, 0);
+			case 'right': return position.translate(0, 1);
+		}
+	});
+
+	editor.selections = positions.map(position => new vscode.Selection(position, position));
+}
+
+function updateCursor() {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) return;
+
+	const cursorStyle = vimMode === 'command' ? vscode.TextEditorCursorStyle.Block : vscode.TextEditorCursorStyle.Line;
+	editor.options.cursorStyle = cursorStyle;
+}
+
+
+export function deactivate() { }
